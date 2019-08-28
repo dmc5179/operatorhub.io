f3eba4f1e800ec4bb1c658775d5c052a46b8f8f9
Dan Clark <danielmclark@gmail.com>
2019-08-28 19:20:51 +0000

    Add steps to set OLM repo and OLM install CMD

--- run.sh
+++ run.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+# If the pod is run with a different repo and command
+# change them before starting the server
+if [[ ! -z $OLM_REPO ]]
+then
+  sed -i "s|const olmRepo =.*|const olmRepo = '$OLM_REPO';|" 'frontend/src/components/InstallModal.js'
+fi
+
+if [[ ! -z $INSTALL_OLM_PATH ]]
+then
+  sed -i "s|const INSTALL_OLM_COMMAND =.*|const INSTALL_OLM_COMMAND = \`curl -sL \$\{olmRepo\}/${INSTALL_OLM_PATH} \| bash -s install.sh\`;|" 'frontend/src/components/InstallModal.js'
+fi
+
 pushd server
 npm run-script server
